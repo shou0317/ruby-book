c2b3ccd568c87395a6a4b191232cce5b335315ec
shou0317 <shou6130@gmail.com>
2024-12-03 15:52:28 +0900

    Create a prototype of convert_length

---
+++ test/convert_length_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+
+def convert_length(length,from,to)
+  unit = {
+    'm' => 1.00,
+    'ft' => 3.28,
+    'in' => 39.37
+  }
+  (length / unit[from] * unit[to]).round(2)
+end
+
+class ConvertLengthTest < Minitest::Test
+  def test_convert_length
+    assert_equal 39.37, convert_length(1, 'm', 'in')
+    assert_equal 0.38, convert_length(15, 'in', 'm')
+    assert_equal 10670.73, convert_length(35000, 'ft', 'm')
+  end
+end

45a44d70f5df67ae4da7f9fe3c5cd90106f0a103
shou0317 <shou6130@gmail.com>
2024-12-04 12:56:57 +0900

    Add test case

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -66,4 +66,16 @@ class GateTest < Minitest::Test
     @umeda.enter(ticket)
     refute @mikuni.exit(ticket)
   end
+
+  def test_gate_umeda_to_mikuni_when_fare_is_enough
+    ticket = Ticket.new(190)
+    @umeda.enter(ticket)
+    assert @mikuni.exit(ticket)
+  end
+
+  def test_gate_juso_to_mikuni_when_fare_is_enough
+    ticket = Ticket.new(160)
+    @juso.enter(ticket)
+    assert @mikuni.exit(ticket)
+  end
 end

2e63ed0e0bc7e0abd9eaf4fb9db2e60d9cc954d0
shou0317 <shou6130@gmail.com>
2024-12-08 11:40:44 +0900

    Create rainbowable program

---
+++ test/rainbowable_test.rb
@@ -0,0 +1,34 @@
+require 'minitest/autorun'
+
+module Rainbowable
+  def rainbow
+    count = 0
+    colord_chars = []
+
+    self.to_s.each_char do |char|
+      color = 31 + count % 6
+      colord_chars << "\e[#{color}m#{char}"
+      count += 1
+    end
+
+    colord_chars.push("\e[0m").join
+  end
+end
+
+class RainbowableTest < Minitest::Test
+  def setup
+    String.include Rainbowable
+    Array.include Rainbowable
+  end
+
+  def test_rainbow
+    expected = "\e[31mH\e[32me\e[33ml\e[34ml\e[35mo\e[36m,\e[31m \e[32mw\e[33mo\e[34mr\e[35ml\e[36md\e[31m!\e[0m"
+    assert_equal expected, 'Hello, world!'.rainbow
+
+    expected = "\e[31m[\e[32m1\e[33m,\e[34m \e[35m2\e[36m,\e[31m \e[32m3\e[33m]\e[0m"
+    assert_equal expected, [1,2,3].rainbow
+
+    # puts 'Hello, world!'.rainbow
+    # puts [1,2,3].rainbow
+  end
+end

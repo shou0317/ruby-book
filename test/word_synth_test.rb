f13d0685dd80a4395fc56c586c92d359ca0259ee
shou0317 <shou6130@gmail.com>
2024-12-17 12:03:02 +0900

    Add word_synth_test

---
+++ test/word_synth_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require_relative '../lib/word_synth'
+
+class WordSynthTest < Minitest::Test
+  attr_reader :synth, :text
+
+  def setup
+    @synth = WordSynth.new
+    @text = 'Ruby is fun!'
+  end
+
+  def test_play_witout_effects
+    assert_equal 'Ruby is fun!', synth.play(text)
+  end
+
+  def test_play_with_reverse_effect
+    synth.add_effect(Effects.reverse)
+    assert_equal 'ybuR si !nuf', synth.play(text)
+  end
+
+  def test_play_with_all_effects
+    synth.add_effect(Effects.echo(2))
+    synth.add_effect(Effects.loud(3))
+    synth.add_effect(Effects.reverse)
+    assert_equal '!!!YYBBUURR !!!SSII !!!!!NNUUFF', synth.play(text)
+  end
+end

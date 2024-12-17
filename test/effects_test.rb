c8b7fdd96d0dba2a34ccd78248c95b35d3ee8374
shou0317 <shou6130@gmail.com>
2024-12-17 15:18:26 +0900

    Refactoring effects_test echo

--- test/effects_test.rb
+++ test/effects_test.rb
@@ -10,6 +10,9 @@ class EffectsTest < Minitest::Test
   def test_echo
     effect = Effects.echo(2)
     assert_equal 'RRuubbyy iiss ffuunn!!', effect.call('Ruby is fun!')
+
+    effect = Effects.echo(3)
+    assert_equal 'RRRuuubbbyyy iiisss fffuuunnn!!!', effect.call('Ruby is fun!')
   end
 
   def test_loud

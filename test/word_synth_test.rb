f51e33c25da6b761ce611f547a9d49d04a4226c7
shou0317 <shou6130@gmail.com>
2024-12-17 15:31:40 +0900

    Refactoring word_synth_test

--- test/word_synth_test.rb
+++ test/word_synth_test.rb
@@ -9,6 +9,25 @@ class WordSynthTest < Minitest::Test
     @synth = WordSynth.new
   end
 
+  def test_play_without_effects
+    assert_equal 'Ruby is fun!', synth.play('Ruby is fun!')
+  end
+
+  def test_play_with_reverse_effect
+    synth.add_effect(Effects.reverse)
+    assert_equal 'ybuR si !nuf', synth.play('Ruby is fun!')
+  end
+
+  def test_play_with_echo_effect
+    synth.add_effect(Effects.echo(2))
+    assert_equal 'RRuubbyy iiss ffuunn!!', synth.play('Ruby is fun!')
+  end
+
+  def test_play_with_loud_effect
+    synth.add_effect(Effects.loud(3))
+    assert_equal 'RUBY!!! IS!!! FUN!!!!', synth.play('Ruby is fun!')
+  end
+
   def test_play_with_all_effects
     synth.add_effect(Effects.echo(2))
     synth.add_effect(Effects.loud(3))

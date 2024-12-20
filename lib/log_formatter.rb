3591869da5613ddad869925f7c792694f2ea8d70
shou0317 <shou6130@gmail.com>
2024-12-20 09:56:40 +0900

    Refactoring log_formatter

--- lib/log_formatter.rb
+++ lib/log_formatter.rb
@@ -1,14 +1,17 @@
-class LogFormatter
+module LogFormatter
   def self.format(input)
-    input.map do |hash|
-      case hash
-      in {status: 404 | 500} => h
-        "[ERROR] request_id=#{h[:request_id]}, path=#{h[:path]}, status=#{h[:status]}, error=#{h[:error]}"
-      in {duration: 1000..} => h
-        "[WARN] request_id=#{h[:request_id]}, path=#{h[:path]}, duration=#{h[:duration]}"
-      else
-        "[OK] request_id=#{hash[:request_id]}, path=#{hash[:path]}"
-      end
-    end.join("\n")
+    input.map { |hash| format_log(hash) }.join("\n")
+  end
+
+  def self.format_log(hash)
+    base = "request_id=#{hash[:request_id]}, path=#{hash[:path]}"
+    case hash
+    in {status: 404 | 500} => hash
+      "[ERROR] #{base}, status=#{hash[:status]}, error=#{hash[:error]}"
+    in {duration: 1000..} => hash
+      "[WARN] #{base}, duration=#{hash[:duration]}"
+    else
+      "[OK] #{base}"
+    end
   end
 end
